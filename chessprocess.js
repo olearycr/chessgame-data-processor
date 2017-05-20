0b24fd2f94e92f0216dca4204166d28f8013584e
olearycr <olearycr@mail.uc.edu>
2017-05-20 12:57:21 -0400

    Update chessprocess.js
    Added comments to help explanation of code processes and functions.

--- chessprocess.js
+++ chessprocess.js
@@ -1,4 +1,4 @@
-// Program requires filesystem, readline, mysql, and async.
+// Program requires filesystem, mysql, and async.
 const fs = require("fs"), filename = process.argv[2];
 const mysql = require("mysql");
 const async = require("async");
@@ -8,6 +8,7 @@ let gameDataParsed = [];
 let connection;
 let arrayIndex = 0;
 
+// Main function to populate the database.
 function populateChessGameDatabase() {
   // Ensure a file is being passed as an argument.
   if (process.argv.length < 4) {
@@ -16,17 +17,18 @@ function populateChessGameDatabase() {
     console.error("Error: Not valid arguments.");
     process.exit(1);
   }
-  // MySQL connection properties.
+  // MySQL connection properties. Insert your own properties if you wish to run this.
   connection = mysql.createPool({
     connectionLimit: 500,
-    host: 'DESKTOP-A54L0HN',
+    host: 'local',
     user: 'COLEAR',
-    password: 'chessgame123',
+    password: 'xxx',
     database: 'chessgames',
   });
   this.readTextFile(filename);
 }
 
+// Reads the txt file passed in the arguments and populates an array. Each section of the array has one full game.
 populateChessGameDatabase.prototype.readTextFile = function (filename){
   gameDataArray = fs.readFileSync(filename, 'utf-8').split('\n').filter(Boolean);
   console.log(gameDataArray[0]);
@@ -38,15 +40,18 @@ populateChessGameDatabase.prototype.readTextFile = function (filename){
   });
 }
 
+// Writes the game data to the database.
 populateChessGameDatabase.prototype.writeGame = function (gameTitle, callback){
   async.waterfall([
     function (callback) {
+      // Creates the game's name to keep track of it in the database. This differs from game ID because you can store the name of who played the game.
       let randNum1 = Math.floor((Math.random() * 9999999999) + 1);
       gameTitle = "COLEAR" + randNum1;
       console.log(gameTitle);
       callback(null, gameTitle);
     },
     function (gameTitle, callback) {
+      // First insert game base information into database to be able to track game moves. (See README.md for database design).
       let sqlInsertToGameInfo = "INSERT INTO game_information (??, ??, ??) VALUES (?, NOW(), ?)"
       let sqlInserts = ["game_title", "date_played", "location_played", gameTitle, "Chess.com"];
       sqlInsertToGameInfo = mysql.format(sqlInsertToGameInfo, sqlInserts);
@@ -70,6 +75,7 @@ populateChessGameDatabase.prototype.writeGame = function (gameTitle, callback){
       });
     },
     function (gameTitle, callback) {
+      // To insert game moves, we need a game ID to relate the moves to a single game. This gets the game ID that was just created.
       let sqlGetGameId = "SELECT game_id FROM chessgames.game_information WHERE game_title = '"+ gameTitle +"';";
       console.log(sqlGetGameId);
       connection.query(sqlGetGameId, function (err, result) {
@@ -83,6 +89,7 @@ populateChessGameDatabase.prototype.writeGame = function (gameTitle, callback){
       });
     },
     function (gameTitle, rows, callback) {
+      // Inserts the moves into the database, which is the primary goal of the application.
       let sqlInsertToGameRecord = "INSERT INTO chessgames.game_recorder (game_id, move_number, white_move, black_move) VALUES ?";
       let formattedArrayData = [];
 
@@ -104,6 +111,7 @@ populateChessGameDatabase.prototype.writeGame = function (gameTitle, callback){
         }
       }
 
+      // Commit insert of data into database.
       connection.query(sqlInsertToGameRecord, [formattedArrayData], function (err, result) {
         if (err) throw err;
         console.log("Rows affected for "+ rows[0].game_id +": " + result.affectedRows);
